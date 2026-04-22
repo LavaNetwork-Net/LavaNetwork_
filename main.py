6ac75bcec6d9ad8e717460e366c745cd1616690e
LavaNetwork-Net <byjoschua@gmail.com>
2026-04-22 11:40:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ def get_guild_config(guild_id):
                 "apply": {"enabled": "True", "label": "Apply", "emoji": "📋", "category_id": "", "description": "Staff applications"},
                 "report": {"enabled": "True", "label": "User Report", "emoji": "🚨", "category_id": "", "description": "Report a user"},
                 "bug": {"enabled": "True", "label": "Bug Report", "emoji": "🐛", "category_id": "", "description": "Report a bug"}
+                "beta": {"enabled": "True", "label": "Beta tester", "emoji": "🌋", "category_id": "", "description": "Apply for Beta Tester"}
             }
         },
         "counting": {"enabled": "False", "channel_id": ""},
